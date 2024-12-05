20941b003c3b624fb580f0c47a4cc5ea32069cfa
esfuerzo <dkdldrha@naver.com>
2024-12-05 14:36:17 +0900

    [level 3] Title: 야근 지수, Time: 120.11 ms, Memory: 54.8 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -3,58 +3,46 @@ import java.util.*;
 class Solution {
     public long solution(int n, int[] works) {
         long answer = 0;
-        
-        
-        // Integer[] numbersWrapper = Arrays.stream(works).boxed().toArray(Integer[]::new);
-        // Arrays.sort(numbersWrapper, Collections.reverseOrder());
-        // works = Arrays.stream(numbersWrapper).mapToInt(Integer::intValue).toArray();
-        
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+
+        Integer[] numbersWrapper = Arrays.stream(works).boxed().toArray(Integer[]::new);
+        Arrays.sort(numbersWrapper, Collections.reverseOrder());
+        works = Arrays.stream(numbersWrapper).mapToInt(Integer::intValue).toArray();
+
+
         for(int i=0; i<works.length; i++){
             answer += works[i];
-            pq.offer(works[i]);
         }
         if(answer<=n) return 0;
+        System.out.println(answer);
         long count = 0;
-        while(count<n){
-            int u = pq.poll();
-            pq.offer(u-1);
-            count++;
+        int[] aa = new int[works.length];
+        boolean[] visited = new boolean[works.length];
+        long a = answer - n;
+        while(count<a){
+            for(int i=0; i<works.length; i++){
+                if(visited[i])
+                    continue;
+                if(aa[i] == works[i])
+                    visited[i] = true;
+                else{
+                    aa[i]+=1;
+                    count++;
+                }
+            }
         }
-        // while(!pq.isEmpty()){
-        //     System.out.println(pq.poll());
-        // }
-        // int[] aa = new int[works.length];
-        // boolean[] visited = new boolean[works.length];
-        // long a = answer - n;
-        // while(count<a){
-        //     for(int i=0; i<works.length; i++){
-        //         if(visited[i])
-        //             continue;
-        //         if(aa[i] == works[i])
-        //             visited[i] = true;
-        //         else{
-        //             aa[i]+=1;
-        //             count++;
-        //         }
-        //     }
-        // }
-        // long u = count - a;
-        // for(int i=0; i<u; i++){
-        //     aa[i] -=1;
-        // }
-        // long aaa = 0;
-        // for(int i=0; i<aa.length; i++){
-        //     aaa += aa[i]*aa[i];
-        // }
+        long u = count - a;
+        for(int i=0; i<u; i++){
+            aa[i] -=1;
+        }
         long aaa = 0;
-        while(!pq.isEmpty()){
-            int k = pq.poll();
-            aaa += k*k;
+        for(int i=0; i<aa.length; i++){
+            System.out.println(aa[i]);    
+        }
+        for(int i=0; i<aa.length; i++){
+            aaa += aa[i]*aa[i];
         }
         return aaa;
-        
-        
     }
-    
+
 }
