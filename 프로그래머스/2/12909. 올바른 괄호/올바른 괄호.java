e2bc44b6d63ec39ec83089b3859ee970c99b31e6
김성준 <tjdwns6522@uos.ac.kr>
2025-02-20 12:25:50 +0900

    [level 2] Title: 올바른 괄호, Time: 16.68 ms, Memory: 54.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,17 @@
+import java.util.Stack;
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '('){
+                stack.push(c);
+            }else{
+                if(stack.isEmpty()) return false;
+                stack.pop();
+            }
+        }
+        if(!stack.isEmpty()) return false;
+        return true;
+    }
+}
